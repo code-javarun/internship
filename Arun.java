00e4bbb559a9754b981c6a4bb329ff3b3b4d45ad
Arun S <104417982+code-javarun@users.noreply.github.com>
2023-10-17 16:19:24 +0530

    3rd Arun.java

--- Arun.java
+++ Arun.java
@@ -1,6 +1,6 @@
 class Arun{
     public static void main(String[] args){
         System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("");
     }
 }
